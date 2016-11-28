75d70277bc353d506a62eef284832b2f71da6012
ryonext <ryonext.s@gmail.com>
2016-11-28 22:48:06 +0900

    Settings for Facebook SDK

--- app/src/main/java/com/ryonext/cognitoandroidsample/MainActivity.java
+++ app/src/main/java/com/ryonext/cognitoandroidsample/MainActivity.java
@@ -2,12 +2,14 @@ package com.ryonext.cognitoandroidsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 }
